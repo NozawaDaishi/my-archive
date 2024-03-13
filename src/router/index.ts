8bd7ab78839b316dff5d995b3eb66ba1ccc111be
NozawaDaishi <nozawa171040@gmail.com>
2024-03-13 19:49:37 +0900

    add: vue-router, pinia, eslint, prettier

---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HelloWorld from '../components/HelloWorld.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld,
+  },
+  // 他のルートをここに追加
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+export default router
