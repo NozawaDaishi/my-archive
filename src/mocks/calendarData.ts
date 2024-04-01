731773b24b96c0f0f6f99342f47400b9b06e3444
NozawaDaishi <nozawa171040@gmail.com>
2024-04-01 19:16:04 +0900

    mod: データの修正

--- src/mocks/calendarData.ts
+++ src/mocks/calendarData.ts
@@ -45,44 +45,19 @@ const mockWorkLog: WorkLog[] = [
   },
   {
     work_day: '2024-03-31',
-    works: [
-      // {
-      //   name: '個人開発',
-      //   content: 'My-Archivesの実装',
-      //   start_time: '19:30',
-      //   end_time: '20:30',
-      //   time: '1時間00分',
-      //   message: '',
-      // },
-    ],
+    works: [],
   },
   {
     work_day: '2024-04-01',
     works: [
-      // {
-      //   name: 'Udemy',
-      //   content: 'TypeScript',
-      //   start_time: '13:00',
-      //   end_time: '18:00',
-      //   time: '5時間00分',
-      //   message: '',
-      // },
-      // {
-      //   name: 'NWスペシャリスト',
-      //   content: 'ネスペR5 10ページ',
-      //   start_time: '18:00',
-      //   end_time: '19:30',
-      //   time: '1時間30分',
-      //   message: '',
-      // },
-      // {
-      //   name: '個人開発',
-      //   content: 'My-Archivesの実装',
-      //   start_time: '19:30',
-      //   end_time: '20:30',
-      //   time: '1時間00分',
-      //   message: '',
-      // },
+      {
+        name: '個人開発',
+        content: 'My-Archivesの実装',
+        start_time: '18:30',
+        end_time: '19:30',
+        time: '1時間00分',
+        message: '',
+      },
     ],
   },
   {
