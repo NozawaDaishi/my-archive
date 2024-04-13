a0ef6f7472349e5971d458e6b9a094e3b6596f96
NozawaDaishi <nozawa171040@gmail.com>
2024-04-13 17:28:59 +0900

    add: 作業ログの追加

--- src/mocks/workLog.ts
+++ src/mocks/workLog.ts
@@ -102,7 +102,7 @@ const mockWorkLog: WorkLog[] = [
       },
       {
         name: 'NWスペシャリスト',
-        content: '午前Ⅱ対策用ドキュメントの用意',
+        content: '午前Ⅱ対策ドキュメント準備',
         start_time: '18:40',
         end_time: '19:40',
         time: '1時間00分',
@@ -127,7 +127,7 @@ const mockWorkLog: WorkLog[] = [
     works: [
       {
         name: 'NWスペシャリスト',
-        content: '午前Ⅱ対策用ドキュメント令和5年春',
+        content: '午前Ⅱ対策令和5年春',
         start_time: '14:10',
         end_time: '15:50',
         time: '1時間40分',
@@ -136,11 +136,61 @@ const mockWorkLog: WorkLog[] = [
       {
         name: 'NWスペシャリスト',
         content: 'ネスペR5 午後1問3',
-        start_time: '16:00',
-        end_time: ':',
-        time: '時間分',
+        start_time: '16:20',
+        end_time: '19:00',
+        time: '2時間40分',
         message: '',
       },
+    ],
+  },
+  {
+    work_day: '2024-04-08',
+    works: [],
+  },
+  {
+    work_day: '2024-04-09',
+    works: [],
+  },
+  {
+    work_day: '2024-04-10',
+    works: [],
+  },
+  {
+    work_day: '2024-04-11',
+    works: [
+      {
+        name: 'NWスペシャリスト',
+        content: '午前Ⅱ令和4年春',
+        start_time: '0:30',
+        end_time: '2:00',
+        time: '1時間30分',
+        message: '',
+      },
+    ],
+  },
+  {
+    work_day: '2024-04-12',
+    works: [],
+  },
+  {
+    work_day: '2024-04-13',
+    works: [
+      {
+        name: 'NWスペシャリスト',
+        content: '午前Ⅱ令和3年春~平成28年秋',
+        start_time: '12:51',
+        end_time: '17:232',
+        time: '4時間32分',
+        message: '',
+      },
+      // {
+      //   name: 'NWスペシャリスト',
+      //   content: 'ネスペR4 午後1問1',
+      //   start_time: ':',
+      //   end_time: ':',
+      //   time: '時間分',
+      //   message: '',
+      // },
     ],
   },
 ]
