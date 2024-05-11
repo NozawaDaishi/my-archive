afd111263a082dc03c91d886b4a64cc4a1c88446
NozawaDaishi <nozawa171040@gmail.com>
2024-05-12 00:46:30 +0900

    mod: 環境変数の削除

--- src/router/index.ts
+++ src/router/index.ts
@@ -35,7 +35,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router: Router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes,
 })
 
